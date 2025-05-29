775cb147a8e4a8accd5777e31f76e2992ce0bab7
beihuang <virgolepus@gmail.com>
2025-05-29 15:45:43 +0200

    Fixed the blanket order view.

--- custom_blanket_order/__manifest__.py
+++ custom_blanket_order/__manifest__.py
@@ -4,8 +4,8 @@
     'depends': ['sale', 'product'],
     'data': [
         'security/ir.model.access.csv',
-        'views/blanket_order_views.xml',
         'views/blanket_order_menu.xml',
+        'views/blanket_order_views.xml',
     ],
     'installable': True,
 }
